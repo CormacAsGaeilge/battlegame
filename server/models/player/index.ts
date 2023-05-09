c70f07ae273cebbbf8fd35d494f619a4ca64c015
CormacAsGaeilge <cwhite788@gmail.com>
2023-05-09 20:48:59 +0100

    Implemented defaults for Player

--- server/models/player/index.ts
+++ server/models/player/index.ts
@@ -6,9 +6,24 @@ export interface Player {
     startHP: number; // between 95 and 105
     level: number; // between 1 and 40
     experience: number; // 0
+    nextLvl: number; // 0
     currentBA: number; // 0
     currentHP: number; // 0
     luck: number; // between 0.004 and 0.009
     totalStolenGold: number; // 0
 }
   
+export const START_BASE_ATTACK = 70;
+export const START_HIT_POINTS = 100;
+export const MIN_LEVEL = 1;
+export const MAX_LEVEL = 40;
+export const START_EXP = 0;
+export const NEXT_LVL = 800;
+export const MIN_LUCK = 0.004;
+export const MAX_LUCK = 0.009;
+export const BA_LVL_MULTIPLIER = 1.1934;
+export const HP_LVL_MULTIPLIER = 1.2735;
+export const LUCK_LVL_MULTIPLIER = 1.05025;
+export const EXP_LVL_MULTIPLIER = 1.1415;
+export const START_GOLD = 5000;
+
--- server/services/dynamoDb/player/index.ts
+++ server/services/dynamoDb/player/index.ts
@@ -1,9 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
-import { Player } from "../../../models/player";
+import { MAX_LUCK, MIN_LEVEL, MIN_LUCK, NEXT_LVL, Player, START_BASE_ATTACK, START_EXP, START_GOLD, START_HIT_POINTS } from "../../../models/player";
 
 export class PlayerService {
-
-    private static readonly START_GOLD: number = 5000;
 
     // fake db
   private players: Player[] = [
@@ -18,7 +16,8 @@ export class PlayerService {
       currentBA: 0,
       currentHP: 0,
       luck: 0.004,
-      totalStolenGold: 0
+      totalStolenGold: 0,
+      nextLvl: 0
     },
     {
       uuid: '83ebc977-00a1-40ed-8aab-e53385633043',
@@ -31,7 +30,8 @@ export class PlayerService {
       currentBA: 0,
       currentHP: 0,
       luck: 0.006,
-      totalStolenGold: 0
+      totalStolenGold: 0,
+      nextLvl: 0
     },
     {
       uuid: '8034cbd5-bc2c-4448-a92c-b8d4c75a137a',
@@ -44,7 +44,8 @@ export class PlayerService {
       currentBA: 0,
       currentHP: 0,
       luck: 0.007,
-      totalStolenGold: 0
+      totalStolenGold: 0,
+      nextLvl: 0
     },
     {
       uuid: '68c900a9-5703-43bc-9514-4184526f3300',
@@ -57,7 +58,8 @@ export class PlayerService {
       currentBA: 0,
       currentHP: 0,
       luck: 0.005,
-      totalStolenGold: 0
+      totalStolenGold: 0,
+      nextLvl: 0
     },
     {
       uuid: '77d53113-f256-4b0f-a7af-3f64add60493',
@@ -70,7 +72,8 @@ export class PlayerService {
       currentBA: 0,
       currentHP: 0,
       luck: 0.005,
-      totalStolenGold: 0
+      totalStolenGold: 0,
+      nextLvl: 0
     },
     {
       uuid: '7dabf7f9-0f0a-46f0-aa52-d806babd1779',
@@ -83,7 +86,8 @@ export class PlayerService {
       currentBA: 0,
       currentHP: 0,
       luck: 0.004,
-      totalStolenGold: 0
+      totalStolenGold: 0,
+      nextLvl: 0
     },
     {
       uuid: 'd6465039-5323-4902-9610-93b912c8a5aa',
@@ -96,7 +100,8 @@ export class PlayerService {
       currentBA: 0,
       currentHP: 0,
       luck: 0.009,
-      totalStolenGold: 0
+      totalStolenGold: 0,
+      nextLvl: 0
     },
     {
       uuid: '2eb1f69b-fa4c-4a6f-ad96-8d9428d97859',
@@ -109,7 +114,8 @@ export class PlayerService {
       currentBA: 0,
       currentHP: 0,
       luck: 0.004,
-      totalStolenGold: 0
+      totalStolenGold: 0,
+      nextLvl: 0
     },
     {
       uuid: '38a5adc5-77b8-4e17-9923-9a42c9053ea6',
@@ -122,7 +128,8 @@ export class PlayerService {
       currentBA: 0,
       currentHP: 0,
       luck: 0.007,
-      totalStolenGold: 0
+      totalStolenGold: 0,
+      nextLvl: 0
     },
     {
       uuid: '4451bf81-1616-4cf3-82e3-b2ed97888171',
@@ -135,7 +142,8 @@ export class PlayerService {
       currentBA: 0,
       currentHP: 0,
       luck: 0.008,
-      totalStolenGold: 0
+      totalStolenGold: 0,
+      nextLvl: 0
     }
   ];
 
@@ -148,21 +156,22 @@ export class PlayerService {
         const newPlayer: Player = {
             uuid: uuidv4(),
             name: name,
-            gold: PlayerService.START_GOLD,
-            startBA: 70,
-            startHP: 100,
-            level: 1,
-            experience: 0,
-            currentBA: 70,
-            currentHP: 100,
+            gold: START_GOLD,
+            startBA: START_BASE_ATTACK,
+            startHP: START_HIT_POINTS,
+            level: MIN_LEVEL,
+            experience: START_EXP,
+            currentBA: START_BASE_ATTACK,
+            currentHP: START_HIT_POINTS,
             luck: this.generateRandomLuck(),
-            totalStolenGold: 0
+            totalStolenGold: START_GOLD,
+            nextLvl: NEXT_LVL
         };
         this.players.push(newPlayer);
         return newPlayer;
     }
 
     private generateRandomLuck(): number {
-        return 0.004 + (Math.random() * 0.009); // random between 0.004 ande 0.009
+        return MIN_LUCK + (Math.random() * MAX_LUCK); // random between 0.004 ande 0.009
     }
 }
