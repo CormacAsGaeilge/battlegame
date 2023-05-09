5f2a722c4d1cc7e22b5a4dac79c7d73ab11b77c3
CormacAsGaeilge <cwhite788@gmail.com>
2023-05-10 00:52:09 +0100

    Update home.component.ts

--- client/BattleGame/src/app/home/home.component.ts
+++ client/BattleGame/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayerService } from '../player.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Player } from '../models/player';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  newPlayer?: Player;
 
-  constructor() { }
+  playerForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.maxLength(20)
+    ]),
+  });
 
-  ngOnInit(): void {
+  constructor(private playerService: PlayerService) { }
+
+  ngOnInit(): void {}
+
+  submit(){
+    if(!this.playerForm.value.name) return;
+
+    this.playerService.createPlayer(this.playerForm.value.name).subscribe(
+      (data) => {
+        this.newPlayer = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-
 }
