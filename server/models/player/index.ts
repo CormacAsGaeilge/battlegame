52c167fe6b6958ea72743854406a364dc34b95ff
CormacAsGaeilge <cwhite788@gmail.com>
2023-05-09 20:10:06 +0100

    Added player model and uuid

---
+++ server/models/player/index.ts
@@ -0,0 +1,14 @@
+export interface Player {
+    uuid: string; // random uuid
+    name: string; // max length 20
+    gold: number; // max 1000000000
+    startBA: number; // between 65 and 75
+    StartHP: number; // between 95 and 105
+    level: number; // between 1 and 40
+    experience: number; // 0
+    currentBA: number; // 0
+    currentHP: number; // 0
+    luck: number; // between 0.004 and 0.009
+    totalStolenGold: number; // 0
+}
+  
